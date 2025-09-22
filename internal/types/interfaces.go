fc3977e5de760ad6b9eec3df5899470dfd213506
Marsboy02 <marsboy0619@gmail.com>
2025-09-22 23:19:24 +0900

    docs: types 파일 주석 설정

--- internal/types/interfaces.go
+++ internal/types/interfaces.go
@@ -2,13 +2,11 @@ package types
 
 import "go-chat-server/internal/message"
 
-// HubClient represents a client interface for the hub
 type HubClient interface {
 	Send(msg *message.Message)
 	GetUsername() string
 }
 
-// Hub represents the hub that manages all clients
 type Hub interface {
 	Register(client HubClient)
 	Unregister(client HubClient)
