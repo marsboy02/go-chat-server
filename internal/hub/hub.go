fda9be30fb03cd03dd9f1fa5a9577efb21c23838
Marsboy02 <marsboy0619@gmail.com>
2025-09-25 21:36:26 +0900

    style: hub 디렉토리의 주석 설정

--- internal/hub/hub.go
+++ internal/hub/hub.go
@@ -8,25 +8,18 @@ import (
 	"go-chat-server/internal/types"
 )
 
-// Hub maintains the set of active clients and broadcasts messages to the clients
 type Hub struct {
-	// Registered clients
 	clients map[types.HubClient]bool
 
-	// Inbound messages from the clients
 	broadcast chan *message.Message
 
-	// Register requests from the clients
 	register chan types.HubClient
 
-	// Unregister requests from clients
 	unregister chan types.HubClient
 
-	// Mutex to protect concurrent access to clients map
 	mutex sync.RWMutex
 }
 
-// New creates a new Hub
 func New() *Hub {
 	return &Hub{
 		clients:    make(map[types.HubClient]bool),
@@ -36,7 +29,6 @@ func New() *Hub {
 	}
 }
 
-// Run starts the hub's main loop
 func (h *Hub) Run() {
 	for {
 		select {
@@ -45,7 +37,6 @@ func (h *Hub) Run() {
 			h.clients[client] = true
 			h.mutex.Unlock()
 
-			// Send join message to all clients
 			joinMsg := message.NewJoinMessage(client.GetUsername())
 			h.broadcastToAll(joinMsg)
 
@@ -57,7 +48,6 @@ func (h *Hub) Run() {
 				delete(h.clients, client)
 				h.mutex.Unlock()
 
-				// Send leave message to all clients
 				leaveMsg := message.NewLeaveMessage(client.GetUsername())
 				h.broadcastToAll(leaveMsg)
 
@@ -72,22 +62,18 @@ func (h *Hub) Run() {
 	}
 }
 
-// Register adds a client to the hub
 func (h *Hub) Register(client types.HubClient) {
 	h.register <- client
 }
 
-// Unregister removes a client from the hub
 func (h *Hub) Unregister(client types.HubClient) {
 	h.unregister <- client
 }
 
-// Broadcast sends a message to all connected clients
 func (h *Hub) Broadcast(msg *message.Message) {
 	h.broadcast <- msg
 }
 
-// broadcastToAll sends a message to all connected clients
 func (h *Hub) broadcastToAll(msg *message.Message) {
 	h.mutex.RLock()
 	defer h.mutex.RUnlock()
@@ -100,14 +86,12 @@ func (h *Hub) broadcastToAll(msg *message.Message) {
 	}
 }
 
-// GetClientCount returns the number of connected clients
 func (h *Hub) GetClientCount() int {
 	h.mutex.RLock()
 	defer h.mutex.RUnlock()
 	return len(h.clients)
 }
 
-// GetConnectedUsers returns a slice of usernames of all connected users
 func (h *Hub) GetConnectedUsers() []string {
 	h.mutex.RLock()
 	defer h.mutex.RUnlock()
